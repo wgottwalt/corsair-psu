2bb823f8e73b30e67a79cf5e5f9b9e49769516d7
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2021-06-03 10:12:44 +0200

    corsair-psu: fixed SPDX headers

--- corsair-psu.c
+++ corsair-psu.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0+ OR MPL-1.1
 /*
  * corsair-psu.c - Linux driver for Corsair power supplies with HID sensors interface
  * Copyright (C) 2020 Wilken Gottwalt <wilken.gottwalt@posteo.net>

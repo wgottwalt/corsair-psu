70b2777b6aee643c0ba61312582a28ed061c5650
Wilken Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2022-11-03 11:09:59 +0100

    added more testing stuff to enhanced driver
    Started to add changes to support collecting statistics. The hwmon
    subsystem actually seem to support statistics. Removed the old debugfs
    approach.

--- enhanced/corsair-psu.c
+++ enhanced/corsair-psu.c
@@ -10,8 +10,10 @@
 #include <linux/hid.h>
 #include <linux/hwmon.h>
 #include <linux/hwmon-sysfs.h>
+#include <linux/init.h>
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
+#include <linux/kthread.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
@@ -55,6 +57,9 @@
 #define SECONDS_PER_DAY		(SECONDS_PER_HOUR * 24)
 #define RAIL_COUNT		3 /* 3v3 + 5v + 12v */
 #define TEMP_COUNT		2
+#define POLL_DEFAULT_MS		1000
+#define POLL_MIN_MS		500L
+#define POLL_MAX_MS		10000L
 
 #define PSU_CMD_SELECT_RAIL	0x00 /* expects length 2 */
 #define PSU_CMD_RAIL_VOLTS_HCRIT 0x40 /* the rest of the commands expect length 3 */
@@ -81,7 +86,7 @@
 #define L_OUT_VOLTS_12V		"v_out +12v"
 #define L_OUT_VOLTS_5V		"v_out +5v"
 #define L_OUT_VOLTS_3_3V	"v_out +3.3v"
-#define L_IN_AMPS		"curr in"
+#define L_IN_AMPS		"curr in(guessed)"
 #define L_AMPS_12V		"curr +12v"
 #define L_AMPS_5V		"curr +5v"
 #define L_AMPS_3_3V		"curr +3.3v"
@@ -114,13 +119,65 @@ static const char *const label_amps[] = {
 	L_AMPS_3_3V
 };
 
-struct corsairpsu_stats {
-	long temp_max[TEMP_COUNT];
-	long fan_max;
-	long power_total_max;
-	long power_max[RAIL_COUNT];
-	long in_max[RAIL_COUNT];
+struct corsairpsu_properties {
+	char vendor[REPLY_SIZE];
+	char product[REPLY_SIZE];
+	long curr_crit[RAIL_COUNT];
+	long in_crit[RAIL_COUNT];
+	long in_lcrit[RAIL_COUNT];
+	long temp_crit[TEMP_COUNT];
+};
+
+struct corsairpsu_statistics {
+	long curr_in_count;
+	long long curr_in_avg;
+	long curr_in_min;
+	long curr_in_max;
+	long curr_in;
+	long curr_count[RAIL_COUNT];
+	long long curr_avg[RAIL_COUNT];
+	long curr_min[RAIL_COUNT];
 	long curr_max[RAIL_COUNT];
+	long curr[RAIL_COUNT];
+	long fan_count;
+	long long fan_avg;
+	long fan_min;
+	long fan_max;
+	long fan;
+	long in_count;
+	long long in_avg;
+	long in_min;
+	long in_max;
+	long in;
+	long in_rail_count[RAIL_COUNT];
+	long long in_rail_avg[RAIL_COUNT];
+	long in_rail_min[RAIL_COUNT];
+	long in_rail_max[RAIL_COUNT];
+	long in_rail[RAIL_COUNT];
+	long power_count[RAIL_COUNT];
+	long long power_avg[RAIL_COUNT];
+	long power_min[RAIL_COUNT];
+	long power_max[RAIL_COUNT];
+	long power[RAIL_COUNT];
+	long power_total_count;
+	long long power_total_avg;
+	long power_total_min;
+	long power_total_max;
+	long power_total;
+	long temp_count[TEMP_COUNT];
+	long long temp_avg[TEMP_COUNT];
+	long temp_min[TEMP_COUNT];
+	long temp_max[TEMP_COUNT];
+	long temp[TEMP_COUNT];
+};
+
+struct corsairpsu_settings {
+	u16 sample_tick_ms;
+	u8 temp_crit_support;
+	u8 in_crit_support;
+	u8 in_lcrit_support;
+	u8 curr_crit_support;
+	bool in_curr_cmd_support; /* not all commands are supported on every PSU */
 };
 
 struct corsairpsu_data {
@@ -129,20 +186,18 @@ struct corsairpsu_data {
 	struct dentry *debugfs;
 	struct completion wait_completion;
 	struct mutex lock; /* for locking access to cmd_buffer */
-	struct corsairpsu_stats stats;
-	u8 *cmd_buffer;
-	char vendor[REPLY_SIZE];
-	char product[REPLY_SIZE];
-	long temp_crit[TEMP_COUNT];
-	long in_crit[RAIL_COUNT];
-	long in_lcrit[RAIL_COUNT];
-	long curr_crit[RAIL_COUNT];
-	u8 temp_crit_support;
-	u8 in_crit_support;
-	u8 in_lcrit_support;
-	u8 curr_crit_support;
-	bool in_curr_cmd_support; /* not all commands are supported on every PSU */
+	struct task_struct *sample_task;
+	struct corsairpsu_properties props;
+	struct corsairpsu_statistics stats;
+	struct corsairpsu_settings setup;
+	u8 cmd_buffer[CMD_BUFFER_SIZE];
 };
+
+static int polling = 0;
+module_param(polling, int, 0444);
+MODULE_PARM_DESC(polling, "enables continuous polling (default: 0)");
+
+static int corsairpsu_get_value(struct corsairpsu_data *, u8, u8, long *);
 
 /* some values are SMBus LINEAR11 data which need a conversion */
 static int corsairpsu_linear11_to_int(const u16 val, const int scale)
@@ -152,6 +207,38 @@ static int corsairpsu_linear11_to_int(const u16 val, const int scale)
 	const int result = mant * scale;
 
 	return (exp >= 0) ? (result << exp) : (result >> -exp);
+}
+
+/*
+ * it is possible to calc "efficiency" to some degree based on the efficiency curves from the
+ * manuals of every supported PSU, currently I stick to the average
+ */
+static int corsairpsu_calc_in_curr(struct corsairpsu_data *priv, long *val)
+{
+	long watts = 0;
+	long volts = 0;
+	int err = 0;
+
+	if (polling <= 0) {
+		err = corsairpsu_get_value(priv, PSU_CMD_TOTAL_WATTS, 0, &watts);
+		if (err < 0)
+			return err;
+
+		err = corsairpsu_get_value(priv, PSU_CMD_IN_VOLTS, 0, &volts);
+		if (err < 0)
+			return err;
+	} else {
+		watts = priv->stats.power_total;
+		volts = priv->stats.in;
+	}
+
+	if (watts <= 0 && volts <= 0)
+		return -EOPNOTSUPP;
+
+	/* most of these PSUs have an average efficiency of 92%, so put it in there */
+	*val = watts / volts / 100 * 108;
+
+	return 0;
 }
 
 static int corsairpsu_usb_cmd(struct corsairpsu_data *priv, u8 p0, u8 p1, u8 p2, void *data)
@@ -202,11 +289,11 @@ static int corsairpsu_fwinfo(struct corsairpsu_data *priv)
 {
 	int ret;
 
-	ret = corsairpsu_usb_cmd(priv, 3, PSU_CMD_VEND_STR, 0, priv->vendor);
+	ret = corsairpsu_usb_cmd(priv, 3, PSU_CMD_VEND_STR, 0, priv->props.vendor);
 	if (ret < 0)
 		return ret;
 
-	ret = corsairpsu_usb_cmd(priv, 3, PSU_CMD_PROD_STR, 0, priv->product);
+	ret = corsairpsu_usb_cmd(priv, 3, PSU_CMD_PROD_STR, 0, priv->props.product);
 	if (ret < 0)
 		return ret;
 
@@ -217,7 +304,8 @@ static int corsairpsu_request(struct corsairpsu_data *priv, u8 cmd, u8 rail, voi
 {
 	int ret;
 
-	mutex_lock(&priv->lock);
+	if (polling <= 0)
+		mutex_lock(&priv->lock);
 	switch (cmd) {
 	case PSU_CMD_RAIL_VOLTS_HCRIT:
 	case PSU_CMD_RAIL_VOLTS_LCRIT:
@@ -236,7 +324,8 @@ static int corsairpsu_request(struct corsairpsu_data *priv, u8 cmd, u8 rail, voi
 	ret = corsairpsu_usb_cmd(priv, 3, cmd, 0, data);
 
 cmd_fail:
-	mutex_unlock(&priv->lock);
+	if (polling <= 0)
+		mutex_unlock(&priv->lock);
 	return ret;
 }
 
@@ -290,6 +379,72 @@ static int corsairpsu_get_value(struct corsairpsu_data *priv, u8 cmd, u8 rail, l
 	return ret;
 }
 
+static void corsairpsu_poll_values(struct corsairpsu_data *priv)
+{
+	long val = 0;
+	int i = 0;
+	int err = 0;
+
+	err = corsairpsu_get_value(priv, PSU_CMD_TEMP0, 0, &val);
+	if (!err) {
+		++priv->stats.temp_count[0];
+		priv->stats.temp[0] = val;
+	}
+	err = corsairpsu_get_value(priv, PSU_CMD_TEMP1, 1, &val);
+	if (!err) {
+		++priv->stats.temp_count[1];
+		priv->stats.temp[1] = val;
+	}
+
+	err = corsairpsu_get_value(priv, PSU_CMD_FAN, 0, &val);
+	if (!err) {
+		++priv->stats.fan_count;
+		priv->stats.fan = val;
+	}
+
+	err = corsairpsu_get_value(priv, PSU_CMD_TOTAL_WATTS, 0, &val);
+	if (!err) {
+		++priv->stats.power_total_count;
+		priv->stats.power_total = val;
+	}
+	for (i = 0; i < RAIL_COUNT; ++i) {
+		err = corsairpsu_get_value(priv, PSU_CMD_RAIL_WATTS, i, &val);
+		if (!err) {
+			++priv->stats.power_count[i];
+			priv->stats.power[i] = val;
+		}
+	}
+
+	err = corsairpsu_get_value(priv, PSU_CMD_IN_VOLTS, 0, &val);
+	if (!err) {
+		++priv->stats.in_count;
+		priv->stats.in = val;
+	}
+	for (i = 0; i < RAIL_COUNT; ++i) {
+		err = corsairpsu_get_value(priv, PSU_CMD_RAIL_VOLTS, i, &val);
+		if (!err) {
+			++priv->stats.in_rail_count[i];
+			priv->stats.in_rail[i] = val;
+		}
+	}
+
+	if (priv->setup.in_curr_cmd_support)
+		err = corsairpsu_get_value(priv, PSU_CMD_IN_AMPS, 0, &val);
+	else
+		err = corsairpsu_calc_in_curr(priv, &val);
+	if (!err) {
+		++priv->stats.curr_in_count;
+		priv->stats.curr_in = val;
+	}
+	for (i = 0; i < RAIL_COUNT; ++i) {
+		err = corsairpsu_get_value(priv, PSU_CMD_RAIL_AMPS, i, &val);
+		if (!err) {
+			++priv->stats.curr_count[i];
+			priv->stats.curr[i] = val;
+		}
+	}
+}
+
 static void corsairpsu_get_criticals(struct corsairpsu_data *priv)
 {
 	long tmp;
@@ -297,25 +452,25 @@ static void corsairpsu_get_criticals(struct corsairpsu_data *priv)
 
 	for (rail = 0; rail < TEMP_COUNT; ++rail) {
 		if (!corsairpsu_get_value(priv, PSU_CMD_TEMP_HCRIT, rail, &tmp)) {
-			priv->temp_crit_support |= BIT(rail);
-			priv->temp_crit[rail] = tmp;
+			priv->setup.temp_crit_support |= BIT(rail);
+			priv->props.temp_crit[rail] = tmp;
 		}
 	}
 
 	for (rail = 0; rail < RAIL_COUNT; ++rail) {
 		if (!corsairpsu_get_value(priv, PSU_CMD_RAIL_VOLTS_HCRIT, rail, &tmp)) {
-			priv->in_crit_support |= BIT(rail);
-			priv->in_crit[rail] = tmp;
+			priv->setup.in_crit_support |= BIT(rail);
+			priv->props.in_crit[rail] = tmp;
 		}
 
 		if (!corsairpsu_get_value(priv, PSU_CMD_RAIL_VOLTS_LCRIT, rail, &tmp)) {
-			priv->in_lcrit_support |= BIT(rail);
-			priv->in_lcrit[rail] = tmp;
+			priv->setup.in_lcrit_support |= BIT(rail);
+			priv->props.in_lcrit[rail] = tmp;
 		}
 
 		if (!corsairpsu_get_value(priv, PSU_CMD_RAIL_AMPS_HCRIT, rail, &tmp)) {
-			priv->curr_crit_support |= BIT(rail);
-			priv->curr_crit[rail] = tmp;
+			priv->setup.curr_crit_support |= BIT(rail);
+			priv->props.curr_crit[rail] = tmp;
 		}
 	}
 }
@@ -324,7 +479,18 @@ static void corsairpsu_check_cmd_support(struct corsairpsu_data *priv)
 {
 	long tmp;
 
-	priv->in_curr_cmd_support = !corsairpsu_get_value(priv, PSU_CMD_IN_AMPS, 0, &tmp);
+	priv->setup.in_curr_cmd_support = !corsairpsu_get_value(priv, PSU_CMD_IN_AMPS, 0, &tmp);
+}
+
+static umode_t corsairpsu_hwmon_chip_is_visible(const struct corsairpsu_data *priv, u32 attr,
+						int channel)
+{
+	switch (attr) {
+	case hwmon_chip_update_interval:
+		return 0644;
+	default:
+		return 0;
+	}
 }
 
 static umode_t corsairpsu_hwmon_temp_is_visible(const struct corsairpsu_data *priv, u32 attr,
@@ -336,7 +502,7 @@ static umode_t corsairpsu_hwmon_temp_is_visible(const struct corsairpsu_data *pr
 	case hwmon_temp_input:
 	case hwmon_temp_label:
 	case hwmon_temp_crit:
-		if (channel > 0 && !(priv->temp_crit_support & BIT(channel - 1)))
+		if (channel > 0 && !(priv->setup.temp_crit_support & BIT(channel - 1)))
 			res = 0;
 		break;
 	default:
@@ -379,11 +545,11 @@ static umode_t corsairpsu_hwmon_in_is_visible(const struct corsairpsu_data *priv
 	case hwmon_in_input:
 	case hwmon_in_label:
 	case hwmon_in_crit:
-		if (channel > 0 && !(priv->in_crit_support & BIT(channel - 1)))
+		if (channel > 0 && !(priv->setup.in_crit_support & BIT(channel - 1)))
 			res = 0;
 		break;
 	case hwmon_in_lcrit:
-		if (channel > 0 && !(priv->in_lcrit_support & BIT(channel - 1)))
+		if (channel > 0 && !(priv->setup.in_lcrit_support & BIT(channel - 1)))
 			res = 0;
 		break;
 	default:
@@ -400,12 +566,9 @@ static umode_t corsairpsu_hwmon_curr_is_visible(const struct corsairpsu_data *pr
 
 	switch (attr) {
 	case hwmon_curr_input:
-		if (channel == 0 && !priv->in_curr_cmd_support)
-			res = 0;
-		break;
 	case hwmon_curr_label:
 	case hwmon_curr_crit:
-		if (channel > 0 && !(priv->curr_crit_support & BIT(channel - 1)))
+		if (channel > 0 && !(priv->setup.curr_crit_support & BIT(channel - 1)))
 			res = 0;
 		break;
 	default:
@@ -421,6 +584,8 @@ static umode_t corsairpsu_hwmon_ops_is_visible(const void *data, enum hwmon_sens
 	const struct corsairpsu_data *priv = data;
 
 	switch (type) {
+	case hwmon_chip:
+		return corsairpsu_hwmon_chip_is_visible(priv, attr, channel);
 	case hwmon_temp:
 		return corsairpsu_hwmon_temp_is_visible(priv, attr, channel);
 	case hwmon_fan:
@@ -436,25 +601,30 @@ static umode_t corsairpsu_hwmon_ops_is_visible(const void *data, enum hwmon_sens
 	}
 }
 
-static int corsairpsu_calc_in_curr(struct corsairpsu_data *priv, long *val)
+static int corsairpsu_hwmon_chip_read(struct corsairpsu_data *priv, u32 attr, int channel,
+				      long *val)
 {
-	long watts = 0;
-	long volts = 0;
-	int err = 0;
-
-	err = corsairpsu_get_value(priv, PSU_CMD_TOTAL_WATTS, 0, &watts);
-	if (err < 0)
-		return err;
-
-	err = corsairpsu_get_value(priv, PSU_CMD_IN_VOLTS, 0, &volts);
-	if (err < 0)
-		return err;
-
-	if (watts <= 0 && volts <= 0)
+	switch (attr) {
+	case hwmon_chip_update_interval:
+		*val = POLL_DEFAULT_MS;
+		break;
+	default:
 		return -EOPNOTSUPP;
+	}
 
-	/* most of these PSUs have an average efficiency of 92%, so put it in there */
-	*val = watts / volts / 100 * 108;
+	return 0;
+}
+
+static int corsairpsu_hwmon_chip_write(struct corsairpsu_data *priv, u32 attr, int channel,
+				       long val)
+{
+	switch (attr) {
+	case hwmon_chip_update_interval:
+		priv->setup.sample_tick_ms = min(POLL_MAX_MS, max(POLL_MIN_MS, val));
+		break;
+	default:
+		return -EOPNOTSUPP;
+	}
 
 	return 0;
 }
@@ -474,7 +644,7 @@ static int corsairpsu_hwmon_temp_read(struct corsairpsu_data *priv, u32 attr, in
 		break;
 	}
 	case hwmon_temp_crit:
-		*val = priv->temp_crit[channel];
+		*val = priv->props.temp_crit[channel];
 		err = 0;
 		break;
 	default:
@@ -531,11 +701,11 @@ static int corsairpsu_hwmon_in_read(struct corsairpsu_data *priv, u32 attr, int 
 		}
 		break;
 	case hwmon_in_crit:
-		*val = priv->in_crit[channel - 1];
+		*val = priv->props.in_crit[channel - 1];
 		err = 0;
 		break;
 	case hwmon_in_lcrit:
-		*val = priv->in_lcrit[channel - 1];
+		*val = priv->props.in_lcrit[channel - 1];
 		err = 0;
 		break;
 	}
@@ -552,7 +722,7 @@ static int corsairpsu_hwmon_curr_read(struct corsairpsu_data *priv, u32 attr, in
 	case hwmon_curr_input:
 		switch (channel) {
 		case 0:
-			if (priv->in_curr_cmd_support)
+			if (priv->setup.in_curr_cmd_support)
 				err = corsairpsu_get_value(priv, PSU_CMD_IN_AMPS, 0, val);
 			else
 				err = corsairpsu_calc_in_curr(priv, val);
@@ -564,7 +734,7 @@ static int corsairpsu_hwmon_curr_read(struct corsairpsu_data *priv, u32 attr, in
 		}
 		break;
 	case hwmon_curr_crit:
-		*val = priv->curr_crit[channel - 1];
+		*val = priv->props.curr_crit[channel - 1];
 		err = 0;
 		break;
 	default:
@@ -580,6 +750,8 @@ static int corsairpsu_hwmon_ops_read(struct device *dev, enum hwmon_sensor_types
 	struct corsairpsu_data *priv = dev_get_drvdata(dev);
 
 	switch (type) {
+	case hwmon_chip:
+		return corsairpsu_hwmon_chip_read(priv, attr, channel, val);
 	case hwmon_temp:
 		return corsairpsu_hwmon_temp_read(priv, attr, channel, val);
 	case hwmon_fan:
@@ -590,6 +762,19 @@ static int corsairpsu_hwmon_ops_read(struct device *dev, enum hwmon_sensor_types
 		return corsairpsu_hwmon_in_read(priv, attr, channel, val);
 	case hwmon_curr:
 		return corsairpsu_hwmon_curr_read(priv, attr, channel, val);
+	default:
+		return -EOPNOTSUPP;
+	}
+}
+
+static int corsairpsu_hwmon_ops_write(struct device *dev, enum hwmon_sensor_types type, u32 attr,
+				      int channel, long val)
+{
+	struct corsairpsu_data *priv = dev_get_drvdata(dev);
+
+	switch (type) {
+	case hwmon_chip:
+		return corsairpsu_hwmon_chip_write(priv, attr, channel, val);
 	default:
 		return -EOPNOTSUPP;
 	}
@@ -621,12 +806,13 @@ static int corsairpsu_hwmon_ops_read_string(struct device *dev, enum hwmon_senso
 static const struct hwmon_ops corsairpsu_hwmon_ops = {
 	.is_visible	= corsairpsu_hwmon_ops_is_visible,
 	.read		= corsairpsu_hwmon_ops_read,
+	.write		= corsairpsu_hwmon_ops_write,
 	.read_string	= corsairpsu_hwmon_ops_read_string,
 };
 
 static const struct hwmon_channel_info *corsairpsu_info[] = {
 	HWMON_CHANNEL_INFO(chip,
-			   HWMON_C_REGISTER_TZ),
+			   HWMON_C_REGISTER_TZ | HWMON_C_UPDATE_INTERVAL),
 	HWMON_CHANNEL_INFO(temp,
 			   HWMON_T_INPUT | HWMON_T_LABEL | HWMON_T_CRIT,
 			   HWMON_T_INPUT | HWMON_T_LABEL | HWMON_T_CRIT),
@@ -700,7 +886,7 @@ static int vendor_show(struct seq_file *seqf, void *unused)
 {
 	struct corsairpsu_data *priv = seqf->private;
 
-	seq_printf(seqf, "%s\n", priv->vendor);
+	seq_printf(seqf, "%s\n", priv->props.vendor);
 
 	return 0;
 }
@@ -710,7 +896,7 @@ static int product_show(struct seq_file *seqf, void *unused)
 {
 	struct corsairpsu_data *priv = seqf->private;
 
-	seq_printf(seqf, "%s\n", priv->product);
+	seq_printf(seqf, "%s\n", priv->props.product);
 
 	return 0;
 }
@@ -732,36 +918,6 @@ static int ocpmode_show(struct seq_file *seqf, void *unused)
 }
 DEFINE_SHOW_ATTRIBUTE(ocpmode);
 
-static int vrm_temp_max_show(struct seq_file *seqf, void *unused)
-{
-	struct corsairpsu_data *priv = seqf->private;
-
-	seq_printf(seqf, "%ld\n", priv->stats.temp_max[0]);
-
-	return 0;
-}
-DEFINE_SHOW_ATTRIBUTE(vrm_temp_max);
-
-static int case_temp_max_show(struct seq_file *seqf, void *unused)
-{
-	struct corsairpsu_data *priv = seqf->private;
-
-	seq_printf(seqf, "%ld\n", priv->stats.temp_max[1]);
-
-	return 0;
-}
-DEFINE_SHOW_ATTRIBUTE(case_temp_max);
-
-static int psu_fan_max_show(struct seq_file *seqf, void *unused)
-{
-	struct corsairpsu_data *priv = seqf->private;
-
-	seq_printf(seqf, "%ld\n", priv->stats.fan_max);
-
-	return 0;
-}
-DEFINE_SHOW_ATTRIBUTE(psu_fan_max);
-
 static void corsairpsu_debugfs_init(struct corsairpsu_data *priv)
 {
 	char name[32];
@@ -774,9 +930,6 @@ static void corsairpsu_debugfs_init(struct corsairpsu_data *priv)
 	debugfs_create_file("vendor", 0444, priv->debugfs, priv, &vendor_fops);
 	debugfs_create_file("product", 0444, priv->debugfs, priv, &product_fops);
 	debugfs_create_file("ocpmode", 0444, priv->debugfs, priv, &ocpmode_fops);
-	debugfs_create_file("vrm_temp_max", 0444, priv->debugfs, priv, &vrm_temp_max_fops);
-	debugfs_create_file("case_temp_max", 0444, priv->debugfs, priv, &case_temp_max_fops);
-	debugfs_create_file("psu_fan_max", 0444, priv->debugfs, priv, &psu_fan_max_fops);
 }
 
 #else
@@ -794,10 +947,6 @@ static int corsairpsu_probe(struct hid_device *hdev, const struct hid_device_id 
 
 	priv = devm_kzalloc(&hdev->dev, sizeof(struct corsairpsu_data), GFP_KERNEL);
 	if (!priv)
-		return -ENOMEM;
-
-	priv->cmd_buffer = devm_kmalloc(&hdev->dev, CMD_BUFFER_SIZE, GFP_KERNEL);
-	if (!priv->cmd_buffer)
 		return -ENOMEM;
 
 	ret = hid_parse(hdev);
